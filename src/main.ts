2c11e6b12ad623e5e637067db7160c8c07c35047
Zin Min Htet <zetminhtin@gmail.com>
2022-05-11 01:53:07 +0630

    added cookie-session

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+var cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true
-  }));
+  app.use(
+    cookieSession({
+      keys: ['secret'],
+    }),
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(5000);
 }
 bootstrap();
