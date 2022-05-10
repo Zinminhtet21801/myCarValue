3a194fc9d7525a06e9ec6116803c9c794d86c0ca
Zin Min Htet <zetminhtin@gmail.com>
2022-05-11 01:52:57 +0630

    created authservice for signup and signin

---
+++ src/users/auth.service.ts
@@ -0,0 +1,60 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { promisify } from 'util';
+import { UsersService } from './users.service';
+import { createDecipheriv, randomBytes, scrypt as _scrypt } from 'crypto';
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+  async signup(email: string, password: string) {
+    // See if email is in use
+    const users = await this.usersService.find(email);
+    if (users.length > 0) {
+      throw new BadRequestException('Email is already in use');
+    }
+
+    // Hash the users password
+    // Generate a salt
+
+    const salt = randomBytes(8).toString('hex');
+
+    // Hash the salt and password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    // Join the hashed salt and password together
+    const result = salt + '.' + hash.toString('hex');
+
+    // Create a new user and save it
+    const user = await this.usersService.create(email, result);
+
+    // Return the new user
+    return user;
+  }
+
+  async signin(email: string, password: string) {
+    // Find the user with the email
+    const [user] = await this.usersService.find(email);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    // Split the salt and hash from the result
+    const [salt, storedHash] = user.password.split('.');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    
+
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('Invalid password');
+    }
+    
+    return user;
+  }
+}
