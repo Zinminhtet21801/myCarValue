a8a75592f26449fd0b6bf95550547f5059e37300
Zin Min Htet <zetminhtin@gmail.com>
2022-05-11 16:47:52 +0630

    added current-user interceptor

---
+++ src/users/interceptors/current-user.interceptor.ts
@@ -0,0 +1,28 @@
+import {
+  BadRequestException,
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { UsersService } from '../users.service';
+
+@Injectable()
+export class CurrentUserInterceptor implements NestInterceptor {
+  constructor(private usersService: UsersService) {}
+  async intercept(
+    context: ExecutionContext,
+    handler: CallHandler<any>,
+  ): Promise<Observable<any>> {
+    
+    const request = context.switchToHttp().getRequest();
+    const { userId } = request.session || {};
+    
+    const user = await this.usersService.findOne(userId);
+    
+    request.currentUser = user;   
+
+    return handler.handle();
+  }
+}
