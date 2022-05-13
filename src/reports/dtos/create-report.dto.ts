456db3326fdad7227ba58f0497c187e2ad35a227
Zin Min Htet <zetminhtin@gmail.com>
2022-05-13 20:53:42 +0630

    added create-report dto to validate request input

---
+++ src/reports/dtos/create-report.dto.ts
@@ -0,0 +1,41 @@
+import {
+  IsLatitude,
+  IsLongitude,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
+export class CreateReportDto {
+  @IsString()
+  @IsNotEmpty()
+  make: string;
+
+  @IsNotEmpty()
+  model: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(1930)
+  @Max(2050)
+  year: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  mileage: number;
+
+  @IsLongitude()
+  lng: number;
+
+  @IsLatitude()
+  lat: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
+  price: number;
+  
+}
