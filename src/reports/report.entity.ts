536c4a2f8e957519eaa4e594908d5d59a65a241c
Zin Min Htet <zetminhtin@gmail.com>
2022-05-13 20:53:07 +0630

    added report entity

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/users/user.entity';
+import { AfterInsert, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Report {
@@ -7,4 +8,31 @@ export class Report {
 
   @Column()
   price: number;
+
+  @Column()
+  make: string;
+
+  @Column()
+  model: string;
+
+  @Column()
+  year: number;
+
+  @Column()
+  lng: number;
+
+  @Column()
+  lat: number;
+
+  @Column()
+  mileage: number;
+
+  @ManyToOne(type=> User, user=> user.reports)
+  user: User
+
+
+  @AfterInsert()
+  logInsert() {
+    console.log(`Report Inserted ${this.id}`);
+  }
 }
